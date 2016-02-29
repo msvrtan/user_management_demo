967d5daed935c5aaa629ad1bc135fc1a9b40aa3f
Miro Svrtan <miro@mirosvrtan.me>
2016-02-29 16:18:49 +0100

    Code reformat using updated php-cs-fixer rules

--- app/autoload.php
+++ app/autoload.php
@@ -5,7 +5,7 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 error_reporting(error_reporting() & ~E_USER_DEPRECATED);
 
-/**
+/*
  * @var ClassLoader
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
